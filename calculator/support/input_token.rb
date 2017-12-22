de00e688dc87ab86f41fe6ac81a6b05e7c2f6500
gangelo <web.gma@gmail.com>
2017-12-22 17:02:29 -0500

    Remove reference to IOToken as it has been deleted

--- calculator/support/input_token.rb
+++ calculator/support/input_token.rb
@@ -1,8 +1,119 @@
-require_relative "io_token"
-
 module RealPage
    module Calculator
    
-      class InputToken < IOToken; end
+      class InputToken
+         def initialize(token = nil)
+            @token = token
+         end
+
+         #
+         # Instance methods
+
+         def token=(value)
+            @token = value
+         end
+
+         def token
+            InputToken.operand?(@token) ? Float(@token) : @token 
+         end
+
+         def operator?
+            !InputToken.operator?(@token).nil?
+         end
+
+         def operand?
+            InputToken.operand?(@token)
+         end
+
+         def command?
+            InputToken.command?(@token)
+         end
+
+         def valid?
+           InputToken.valid?(@token)
+         end
+
+         def invalid?
+           !InputToken.valid?(@token)
+         end
+
+         def empty?
+            InputToken.empty?(@token)
+         end
+
+         #
+         # Commands
+
+         def view_stack?
+            InputToken.view_stack?(@token)
+         end
+
+         def clear_stack?
+            InputToken.clear_stack?(@token)
+         end
+
+         def quit?
+            InputToken.quit?(@token)
+         end
+
+         #
+         # Class methods
+
+         def self.operators
+            {'+' => '+', '-' => '-', '/' => '/', '*' => '*'}
+         end
+
+         #
+         # Commands:
+         #
+         # s - view the input stack
+         # c - clear the input stack
+         # q - quit
+         def self.commands
+            ['s', 'c', 'q']
+         end
+
+         def self.operator?(token)
+            !InputToken.operators[token].nil?
+         end
+
+         def self.operand?(token)
+            return false if InputToken.operator?(token) || InputToken.command?(token)
+            Float(token) != nil rescue false
+         end
+
+          def self.command?(token)
+            InputToken.commands.include?(token)
+         end
+
+         def self.valid?(token)
+            return false if token.nil? || InputToken.empty?(token)
+            InputToken.operand?(token) || InputToken.operator?(token) || InputToken.command?(token)
+         end
+
+         def self.invalid?(token)
+           !InputToken.valid?(token)
+         end
+
+         def self.empty?(token)
+            token.to_s.empty?
+         end
+
+         #
+         # Commands
+
+         def self.view_stack?(token)
+            token.to_s.downcase == "s"
+         end
+
+         def self.clear_stack?(token)
+            token.to_s.downcase == "c"
+         end
+
+         def self.quit?(token)
+            token.to_s.downcase == "q"
+         end
+      end
+
    end
 end
