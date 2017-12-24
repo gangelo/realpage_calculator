7bf61325937f4cba403b1adbc6ad0f3fb3c7833d
gangelo <web.gma@gmail.com>
2017-12-23 22:23:21 -0500

    Make spec descriptions more descriptive for RPNCalculator#compute function tests

--- spec/rpn_calculator_service_spec.rb
+++ spec/rpn_calculator_service_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "RPNCalculatorService" do
-    before(:each) do
+   before(:each) do
       @calculator_service = RealPage::Calculator::RPNCalculatorService.new
 
       # Change the access modifier on all protected members to public so we can test them.
@@ -21,35 +21,42 @@ describe "RPNCalculatorService" do
             expect(@calculator_service.compute("15 7 1 1 + - / 3 * 2 1 1 + + -").result).to eq(5.0)
          end
 
-         it "should produce the expected output given one token at a time" do
+         it "should produce the expected output given '5 8 + q' one token at a time" do
             expect(@calculator_service.compute("5").result).to eq(5.0)
             expect(@calculator_service.compute("8").result).to eq(8.0)
             expect(@calculator_service.compute("+").result).to eq(13.0)
-            expect(@calculator_service.compute("q").result).to eq("q")
+            expect(@calculator_service.compute("q").result).to eq("")
+         end
 
-            @calculator_service.clear
+         it "should produce the expected output given '5 8 + 13 - q' one token at a time" do
             expect(@calculator_service.compute("5").result).to eq(5.0)
             expect(@calculator_service.compute("8").result).to eq(8.0)
             expect(@calculator_service.compute("+").result).to eq(13.0)
             expect(@calculator_service.compute("13").result).to eq(13.0)
             expect(@calculator_service.compute("-").result).to eq(0.0)
-            expect(@calculator_service.compute("q").result).to eq("q")
+            expect(@calculator_service.compute("q").result).to eq("")
+         end
 
-            @calculator_service.clear
+         it "should produce the expected output given '-3 -2 * 5 + q' one token at a time" do
             expect(@calculator_service.compute("-3").result).to eq(-3.0)
             expect(@calculator_service.compute("-2").result).to eq(-2.0)
             expect(@calculator_service.compute("*").result).to eq(6.0)
             expect(@calculator_service.compute("5").result).to eq(5.0)
             expect(@calculator_service.compute("+").result).to eq(11.0)
-            expect(@calculator_service.compute("q").result).to eq("q")
+            expect(@calculator_service.compute("q").result).to eq("")
+         end
 
-            @calculator_service.clear
+         it "should produce the expected output given '5 9 1 - / q' one token at a time" do
             expect(@calculator_service.compute("5").result).to eq(5.0)
             expect(@calculator_service.compute("9").result).to eq(9.0)
             expect(@calculator_service.compute("1").result).to eq(1.0)
             expect(@calculator_service.compute("-").result).to eq(8.0)
             expect(@calculator_service.compute("/").result).to eq(0.625)
-            expect(@calculator_service.compute("q").result).to eq("q")
+            expect(@calculator_service.compute("q").result).to eq("")
+         end
+
+         it "should return the computed value given a string of tokens that ends with 'quit'" do
+            expect(@calculator_service.compute("5 8 + 13 - q").result).to eq(0.0)
          end
       end
    end # instance methods
