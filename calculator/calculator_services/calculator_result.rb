d75ae17cb5231bf75f39400414248e1e914f175a
gangelo <web.gma@gmail.com>
2017-12-28 11:31:21 -0500

    Add alias #token for #result which makes more sense in the case of errors. Referencing #result in the case of an error is misleading; #token is more appropriate as it will hold the offending token.

--- calculator/calculator_services/calculator_result.rb
+++ calculator/calculator_services/calculator_result.rb
@@ -4,13 +4,30 @@ module RealPage
    module Calculator
    
       # Provides a container for the result returned from a Calculator computation.
-      #
       class CalculatorResult
          public
 
+         # Holds the calculator's computed result or calculator output.
+         # In the case of an error, it will contain the calculator's initial input.
          attr_reader :result
+
+         # An alias for #result which would make more sense in the case if an error
+         # is encountered. In this case, #result would hold the offending input token.
+         # Referencing the offending token via #result is misleading.
+         alias :token :result
+
+         # Holds the calculator error encountered, if any.
          attr_reader :error
 
+         # Initializes this object
+         #
+         # @param [Object] result the calculator's computed result or calculator output.
+         #     In the case of an error, it will contain the calculator's initial input. An
+         #     ArgumentError is raised if result is nil.
+         #
+         # @param [Hash] error the error encountered. This should be an error hasn found in
+         #     RealPage::Calculator::Errors::Calculator.all_errors. An ArgumentError is
+         #     raised if error is not incuded in RealPage::Calculator::Errors::Calculator.all_errors.
          def initialize(result, error = Errors::Calculator::NONE)
             raise ArgumentError, "result parameter is nil" if result.nil?
             raise ArgumentError, "error is invalid" if !Errors::Calculator::all_errors.include?(error)
@@ -19,6 +36,9 @@ module RealPage
             @error = error
          end
 
+         # Returns true if the calculator encountered an error; false otherwise.
+         #
+         # @return [TrueClass, FalseClass]
          def error?
             error != Errors::Calculator::NONE
          end
