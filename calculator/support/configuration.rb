d07ed7ba1e2b18b40008a7f5eac2731d9289d158
gangelo <web.gma@gmail.com>
2017-12-28 15:54:25 -0500

    Add documentation

--- calculator/support/configuration.rb
+++ calculator/support/configuration.rb
@@ -3,39 +3,56 @@ require 'yaml'
 module RealPage
    module Calculator
 
+      # Singleton. Provides a single interface into configuration values.
       class Configuration
 
-         attr_accessor :loaded
+         # Holds an array of valid operators that may be used in computations.
          attr_accessor :operators
+
+         # Holds an array of valid commands that may be used by the interface.
          attr_accessor :commands
 
+         # Initializes an object of this type.
          def initialize
             self.load_config
          end
 
+         # Returns the Configuration instance.
+         #
+         # @return [Configuration] Returns the single(ton) instance of
+         # this Configuraton object.
          def self.instance
             @@instance
          end
 
+         # Retrieves the quit command.
+         #
+         # @return [String] The value that repesents the quit command.
          def quit_command
             self.commands['quit']
          end
 
+         # Retrieves the clear stack command.
+         #
+         # @return [String] The value that repesents the clear stack command.
          def clear_stack_command
             self.commands['clear_stack']
          end
 
+         # Retrieves the view stack command.
+         #
+         # @return [String] The value that repesents the view stack command.
          def view_stack_command
             self.commands['view_stack']
          end
 
          protected
 
+         # Loads the configuration values from the configuration file.
          def load_config
             configuration = YAML::load_file(File.join(__dir__, '../config/calculator_service_config.yml'))
             self.operators = configuration['operators']
             self.commands = configuration['commands']
-            self.loaded = true
          end
 
          @@instance = Configuration.new
--- calculator/support/input_token.rb
+++ calculator/support/input_token.rb
@@ -3,71 +3,119 @@ require_relative '../support/configuration'
 module RealPage
    module Calculator
    
+      # Provides a class that represents an individual, space delimited token received from 
+      # an IOInterface.
       class InputToken
          protected
 
+         # A Hash list of valid operators.
          @@operators = nil
+
+         # A Hash list of valid commands.
          @@commands = nil
          
          public
 
+         # Initializes an object of this type.
+         #
+         # @param [String] token The token value.
          def initialize(token = nil)
             @token = token
          end
 
-         #
+         #--
          # Instance methods
+         #++
 
+         # Sets the token.
+         #
+         # @param [Object] value The value to set #token to.
          def token=(value)
             @token = value
          end
 
+         # Returns the token value.
+         #
+         # @return [Float, Object] Returns the token as a Float
+         # if the token is an operand; returns the token othewise.
          def token
-            InputToken.operand?(@token) ? Float(@token) : @token 
+            InputToken.operand?(@token) ? Float(@token) : @token
          end
 
+         # Returns true or false based on whether or not token is an operator.
+         #
+         # @return [TrueClass, FalseClass] returns true if token is an operator; false otherwise.
          def operator?
             InputToken.operator?(@token)
          end
 
+         # Returns true or false based on whether or not token is an operand.
+         #
+         # @return [TrueClass, FalseClass] returns true if token is an operand; false otherwise.
          def operand?
             InputToken.operand?(@token)
          end
 
+         # Returns true or false based on whether or not token is a command.
+         #
+         # @return [TrueClass, FalseClass] returns true if token is a command; false otherwise.
          def command?
             InputToken.command?(@token)
          end
 
+         # Returns true or false based on whether or not token is an operator, operand or command.
+         #
+         # @return [TrueClass, FalseClass] returns true if token is an operator, operand or command; false otherwise.
          def valid?
            InputToken.valid?(@token)
          end
 
+         # Returns true or false based on whether or not token is a operator, operand or command.
+         #
+         # @return [TrueClass, FalseClass] returns true if token is not an operator, operand or command; false otherwise.
          def invalid?
            !InputToken.valid?(@token)
          end
 
+         # Returns true or false based on whether or not token is nil? or empty?.
+         #
+         # @return [TrueClass, FalseClass] returns true if token is nil? or empty?; false otherwise.
          def empty?
             InputToken.empty?(@token)
          end
 
-         #
+         #--
          # Commands
+         #++
 
+         # Returns true or false based on whether or not token is a view stack command.
+         #
+         # @return [TrueClass, FalseClass] returns true if token is a view stack command; false otherwise.
          def view_stack?
             InputToken.view_stack?(@token)
          end
 
+         # Returns true or false based on whether or not token is a clear stack command.
+         #
+         # @return [TrueClass, FalseClass] returns true if token is a clear stack command; false otherwise.
          def clear_stack?
             InputToken.clear_stack?(@token)
          end
 
+         # Returns true or false based on whether or not token is a quit command.
+         #
+         # @return [TrueClass, FalseClass] returns true if token is a quit command; false otherwise.
          def quit?
             InputToken.quit?(@token)
          end
 
-         #
+         #--
          # Class methods
+         #++
 
+         # Returns a valid list of operators.
+         #
+         # @return [Hash] Returns the list of operators.
          def self.operators
             if @@operators.nil?
                @@operators = Configuration.instance.operators
@@ -75,6 +123,9 @@ module RealPage
             @@operators
          end
 
+         # Returns a valid list of commands.
+         #
+         # @return [Hash] Returns the list of commands.
          def self.commands
             if @@commands.nil?
                @@commands = Configuration.instance.commands
@@ -82,35 +133,71 @@ module RealPage
             @@commands
          end
 
+         # Returns true or false based on whether or not token is an operator.
+         #
+         # @param [Object] token The token to interrogate.
+         #
+         # @return [TrueClass, FalseClass] returns true if token is an operator; false otherwise.
          def self.operator?(token)
             !InputToken.operators[token].nil?
          end
 
+         # Returns true or false based on whether or not token is an operand.
+         #
+         # @param [Object] token The token to interrogate.
+         #
+         # @return [TrueClass, FalseClass] returns true if token is an operand; false otherwise.
          def self.operand?(token)
             return false if InputToken.operator?(token) || InputToken.command?(token)
             Float(token) != nil rescue false
          end
 
+         # Returns true or false based on whether or not token is a command.
+         #
+         # @param [Object] token The token to interrogate.
+         #
+         # @return [TrueClass, FalseClass] returns true if token is a command; false otherwise.
          def self.command?(token)
             !InputToken.commands.key(token).nil?
          end
 
+         # Returns true or false based on whether or not token is an operator, operand or command.
+         #
+         # @param [Object] token The token to interrogate.
+         #
+         # @return [TrueClass, FalseClass] returns true if token is an operator, operand or command; false otherwise.
          def self.valid?(token)
             return false if token.nil? || InputToken.empty?(token)
             InputToken.operand?(token) || InputToken.operator?(token) || InputToken.command?(token)
          end
 
+         # Returns true or false based on whether or not token is a operator, operand or command.
+         #
+         # @param [Object] token The token to interrogate.
+         #
+         # @return [TrueClass, FalseClass] returns true if token is not an operator, operand or command; false otherwise.
          def self.invalid?(token)
            !InputToken.valid?(token)
          end
 
+         # Returns true or false based on whether or not token is nil? or empty?.
+         #
+         # @param [Object] token The token to interrogate.
+         #
+         # @return [TrueClass, FalseClass] returns true if token is nil? or empty?; false otherwise.
          def self.empty?(token)
             token.to_s.empty?
          end
 
-         #
+         #--
          # Commands
+         #++
 
+         # Returns true or false based on whether or not token is a view stack command.
+         #
+         # @param [Object] token The token to interrogate.
+         #
+         # @return [TrueClass, FalseClass] returns true if token is a view stack command; false otherwise.
          def self.view_stack?(token)
             return false unless token.respond_to? :downcase
             token = token.downcase
@@ -118,6 +205,11 @@ module RealPage
             command_value.nil? ? false : token == Configuration.instance.view_stack_command.downcase
          end
 
+         # Returns true or false based on whether or not token is a clear stack command.
+         #
+         # @param [Object] token The token to interrogate.
+         #
+         # @return [TrueClass, FalseClass] returns true if token is a clear stack command; false otherwise.
          def self.clear_stack?(token)
             return false unless token.respond_to? :downcase
             token = token.downcase
@@ -125,6 +217,11 @@ module RealPage
             command_value.nil? ? false : token == Configuration.instance.clear_stack_command.downcase
          end
 
+         # Returns true or false based on whether or not token is a quit command.
+         #
+         # @param [Object] token The token to interrogate.
+         #
+         # @return [TrueClass, FalseClass] returns true if token is a quit command; false otherwise.
          def self.quit?(token)
             return false unless token.respond_to? :downcase
             token = token.downcase
