d1d4b63203786506fb0eecf9ac18a15f7a535916
gangelo <web.gma@gmail.com>
2017-12-27 23:48:27 -0500

    Remove stale 'quit' detection code in CalculatorResult

--- calculator/calculator_services/calculator_result.rb
+++ calculator/calculator_services/calculator_result.rb
@@ -8,23 +8,17 @@ module RealPage
 
          attr_reader :result
          attr_reader :error
-         attr_reader :quit
 
-         def initialize(result, error = CalculatorErrors::NONE, quit = false)
+         def initialize(result, error = CalculatorErrors::NONE)
             raise ArgumentError, "result parameter is nil" if result.nil?
             raise ArgumentError, "error is invalid" if !CalculatorErrors::Errors.include?(error)
 
             @result = result
             @error = error
-            @quit = quit
          end
 
          def error?
             error != CalculatorErrors::NONE
-         end
-
-         def quit?
-            @quit || false
          end
       end
 
